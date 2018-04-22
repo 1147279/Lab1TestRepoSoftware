0ae1f811e4b9bfbcd9227e5cb676a97f05a4bc41
1147279 <1147279@students.wits.ac.za>
2018-04-22 13:08:21 +0200

    New

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ let mainRouter = require("./mainRoutes.js");
 let express = require('express');
 let app = express();
 app.use('/',mainRouter);
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
